3d048255340c5f0e31b1e7f967ce443e7d48f4d8
emily <mle4d@me.com>
2019-08-16 14:33:20 -0700

    file flips

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,15 +1,1 @@
-export const getQuotes = (count = 5) => {
-  return fetch(`futuramaapi.herokuapp.com/api/quotes/count=${count}`)
-    .then(res => {
-      if(!res.ok) throw 'I Apologize for nothing!';
 
-      return res.json();
-    })
-    .then(quotes => {
-      return quotes.map(quote => ({
-        quote: quote.quote,
-        image: quote.PicUrl,
-        name: quote.name
-      }));
-    });
-};
